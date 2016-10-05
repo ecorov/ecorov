781364b1cb7145b2367e7224bc15dff4e25438e3
Huidong Tian <tienhuitung@gmail.com>
2016-10-05 15:55:04 +0200

    Update simpleInstall.sh

--- simpleInstall.sh
+++ simpleInstall.sh
@@ -22,28 +22,20 @@ if [ ! -e /var/www/cam.jpg ]; then
   sudo ln -sf /run/shm/mjpeg/cam.jpg /var/www/cam.jpg
 fi
 
-sudo cp etc/sudoers.d/RPiROV /etc/sudoers.d/
-sudo chmod 440 /etc/sudoers.d/RPiROV
-
 sudo cp -r bin/raspimjpeg /opt/vc/bin/
 sudo chmod 755 /opt/vc/bin/raspimjpeg
 if [ ! -e /usr/bin/raspimjpeg ]; then
   sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
 fi
 
-cat etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
 sudo cp -r etc/raspimjpeg/raspimjpeg /etc/
 sudo chmod 644 /etc/raspimjpeg
 if [ ! -e /var/www/raspimjpeg ]; then
   sudo ln -s /etc/raspimjpeg /var/www/raspimjpeg
 fi
-sudo cp etc/lighttpd/doStuff.py /var/www/html
-sudo cp etc/lighttpd/MS5803.py /var/www/html
-sudo cp etc/lighttpd/hmc5883l.py /var/www/html
+sudo cp etc/lighttpd/* /var/www/html
+sudo chmod 755 /var/www/html/*
 
-sudo chmod 755 /var/www/html/doStuff.py
-sudo chmod 755 /var/www/html/MS5803.py
-sudo chmod 755 /var/www/html/hmc5883l.py
 
 if [ ! -e /usr/bin/pythonRoot ]; then
   sudo cp /usr/bin/python2.7 /usr/bin/pythonRoot
@@ -51,16 +43,16 @@ if [ ! -e /usr/bin/pythonRoot ]; then
 fi
 
 sudo cp etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf
-
-cat etc/rc_local_run/rc.local.1 > etc/rc_local_run/rc.local
 sudo cp -r etc/rc_local_run/rc.local /etc/
 sudo chmod 755 /etc/rc.local
+
+
+
 
 sudo usermod -a -G video www-data
 if [ -e /var/www/uconfig ]; then
   sudo chown www-data:www-data /var/www/uconfig
 fi
-
 ## edit /etc/passwd to make www-data available
 sudo chsh -s /bin/bash www-data
 
