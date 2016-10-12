e84d1b18ab6e07f68389ad237fc33705a1b6a16f
Huidong Tian <tienhuitung@gmail.com>
2016-10-12 13:10:57 +0200

    Update simpleInstall.sh

--- simpleInstall.sh
+++ simpleInstall.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y git python-flup lighttpd python-setuptools python-smbus 
 cd; sudo git clone https://github.com/withr/ecorov.git; 
 cd ecorov; 
 
+## Install raspimjpeg
 sudo mkdir -p /var/www/media
 sudo mknod /var/www/FIFO p
 sudo chmod 666 /var/www/FIFO
@@ -16,40 +17,14 @@ sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
 sudo cp -r etc/raspimjpeg /etc/
 sudo chmod 644 /etc/raspimjpeg
 
-
 sudo mkdir -p /dev/shm/mjpeg
 sudo su -c 'raspimjpeg > /dev/null &' 
 
-
-############################
-
-cd; sudo git clone https://github.com/withr/mjpg-streamer.git; 
-cd mjpg-streamer; 
-sudo make
-sudo make install
-
-
-cd /home/pi/mjpg-streamer
-mjpg_streamer -i "input_file.so -d 0.1 -f /dev/shm/mjpeg -n cam.jpg" -o "output_http.so -w ./www -p 8080"&
-
-
 ## Run at start 
-cd; 
-cd ecorov; 
 sudo cp -r etc/rc.local /etc/
 sudo chmod 755 /etc/rc.local
 
-
-#############################
-# Install RPIO. RPi 3 has problem to instalL RPIO, the solution is using the following repository.
-cd; 
-git clone https://github.com/withr/RPIO-RPi3.git; 
-cd RPIO-RPi3
-sudo python setup.py install; 
-
-
-################
-
+## lighttp & fastcgi
 sudo cp -R www/* /var/www/html/
 sudo cp etc/lighttpd.conf /etc/lighttpd/lighttpd.conf
 
@@ -59,4 +34,25 @@ if [ ! -e /usr/bin/pythonRoot ]; then
 fi
 
 sudo chmod 755 -R /var/www/html
+
+# Install RPIO. RPi 3 has problem to instalL RPIO, the solution is using the following repository.
+cd; 
+git clone https://github.com/withr/RPIO-RPi3.git; 
+cd RPIO-RPi3
+sudo python setup.py install;
+
+## Install mjpg-streamer
+cd; sudo git clone https://github.com/withr/mjpg-streamer.git; 
+cd mjpg-streamer; 
+sudo make
+sudo make install
+
+cd /home/pi/mjpg-streamer
+mjpg_streamer -i "input_file.so -d 0.1 -f /dev/shm/mjpeg -n cam.jpg" -o "output_http.so -w ./www -p 8080"&
+
+
+ 
+
+
+
 
