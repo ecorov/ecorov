cc8b8847d20249bb84f9c537232adf9187ff46fc
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2017-08-03 13:55:45 +0200

    remove php files

--- www/cmd_pipe.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define('BASE_DIR', dirname(__FILE__));
-   require_once(BASE_DIR.'/config.php');
-
-   
-   $pipe = fopen("FIFO","w");
-   fwrite($pipe, $_GET["cmd"]);
-   fclose($pipe);
-?>
