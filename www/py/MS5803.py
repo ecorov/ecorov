0f10e0ced02529a126451cf143b6a3cc21bb0a46
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2017-08-03 14:05:42 +0200

    remove php files

--- www/py/MS5803.py
+++ www/py/MS5803.py
@@ -2,22 +2,22 @@ import smbus,time,struct
 
 class MS5803:
     osr = {256:0,512:2,1024:4,2048:6,4096:8}
-    def __init__(self,address=0x76,bus=1):
-        self.bus = smbus.SMBus(bus)
-        self.address = address
+    def __init__(self):
+        self.bus = smbus.SMBus(1)
+        self.addr = 0x76
         self.reset()
         time.sleep(0.05)
-        self._C = self._read_prom()
+        self._C = self.read_prom()
     def __enter__(self):
         return self
     def __exit__(self,*ignored):
         return True
     def reset(self):
-        self.bus.write_byte(self.address,0x1E)
+        self.bus.write_byte(self.addr,0x1E)
         time.sleep(0.1)
     def read(self,osr=4096):
-        D1 = self._raw_pressure(osr=osr)
-        D2 = self._raw_temperature(osr=osr)
+        D1 = self.raw_pressure(osr=osr)
+        D2 = self.raw_temperature(osr=osr)
         C = self._C
         dT = D2 - C[5]*(2**8)
         TEMP = 2000L + dT*C[6]/(2**23)
@@ -37,7 +37,6 @@ class MS5803:
         TEMP = TEMP - T2
         OFF = OFF - OFF2
         SENS = SENS - SENS2
-        # - - -
         P = (long(D1)*SENS/(2**21) - OFF)/(2**15)
         TEMP = TEMP/100.
         P = P/10.
@@ -46,21 +45,21 @@ class MS5803:
         if r is None:
             r = self.read()
         return '{} kPa, {} Deg.C'.format(r['mbar'],r['temp'])
-    def _read_prom(self):
-        tmp = [self.bus.read_byte_data(self.address,i) for i in range(0xA0,0xAE+1)]
+    def read_prom(self):
+        tmp = [self.bus.read_byte_data(self.addr,i) for i in range(0xA0,0xAE+1)]
         C = struct.unpack('>HHHHHHHB',''.join([chr(c) for c in tmp]))
         return C
-    def _raw_pressure(self,osr=4096):
-        self.bus.write_byte(self.address,0x40 + self.osr[osr])
+    def raw_pressure(self,osr=4096):
+        self.bus.write_byte(self.addr,0x40 + self.osr[osr])
         time.sleep(0.1)
-        tmp = self.bus.read_i2c_block_data(self.address,0,3)
+        tmp = self.bus.read_i2c_block_data(self.addr,0,3)
         tmp.insert(0,0)
         D1 = struct.unpack('>I',''.join([chr(c) for c in tmp]))[0]
         return D1
-    def _raw_temperature(self,osr=4096):
-        self.bus.write_byte(self.address,0x50 + self.osr[osr])
+    def raw_temperature(self,osr=4096):
+        self.bus.write_byte(self.addr,0x50 + self.osr[osr])
         time.sleep(0.1)
-        tmp = self.bus.read_i2c_block_data(self.address,0,3)
+        tmp = self.bus.read_i2c_block_data(self.addr,0,3)
         tmp.insert(0,0)
         D2 = struct.unpack('>I',''.join([chr(c) for c in tmp]))[0]
         return D2
