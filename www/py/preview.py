6d3be463134ded446a5c1468e4275c6fa9c17e79
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2017-08-11 17:14:18 +0200

    include preview.py to ecorov.py

---
+++ www/py/preview.py
@@ -0,0 +1,58 @@
+import time, re, subprocess
+
+template = """
+<html>
+    <head>
+        <title>ecoROV preview</title>
+        <style>
+            {style}
+        </style>
+    </head>
+    <body>
+        <a class="menu-item" style="float: right;" href="/index.html"><h2>HOME</h2></a>
+        <br>{disk}<br><br>
+        {img_grid}
+    </body>
+</html>
+"""
+
+css = """
+    .floated_img{float: left; margin: 10px;}
+    .thumb{width: 256px;}
+   
+"""
+
+
+img_blk = """
+        <div class="floated_img">
+            <a target="_blank" href="{doc_org}"><img src="{img_th}" class="thumb"></a>
+            <p>{doc_name}</p>
+        </div> """
+
+        
+        
+def update_preview():
+    df_Disk  = re.sub(r"/dev/root *| /\n","", subprocess.check_output("df -h | grep root", shell=True))
+    df_disk  = re.split(" +", df_Disk)
+    df_used  = "Total size: <b>%s</b>; Used: <b>%s</b>; Available: <b>%s</b>; Percentage: <b>%s</b>;"  % tuple(df_disk)
+    img_ths  = subprocess.check_output("cd /var/www/ && find media -type f -name *.th.jpg", shell=True).split('\n')[:-1]
+    img_ths.sort(reverse = True)
+    doc_orgs = [re.sub(r'\..{5}\.th\.jpg$', "", img) for img in img_ths]
+    img_blks = [img_blk.format(img_th = img_ths[i], doc_org = doc_orgs[i], doc_name = re.sub(r'^media/', "", doc_orgs[i])) for i in range(len(img_ths))]
+    pag_html = template.format(style = css, disk = df_used, img_grid = ''.join(img_blks))
+    with open("/var/www/preview.html", "w") as f:
+        f.write(pag_html)
+        f.close()
+        
+## Initialize
+update_preview()        
+
+state_init = subprocess.check_output("cd /var/www/ && find media -type f -name *.th.jpg", shell=True).split('\n')[:-1]
+while True:
+    state_now = subprocess.check_output("cd /var/www/ && find media -type f -name *.th.jpg", shell=True).split('\n')[:-1]
+    if state_now != state_init:
+        state_init = state_now
+        update_preview()
+    time.sleep(2)
+
+ 
